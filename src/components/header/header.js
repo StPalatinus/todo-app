6b9935ae31dfe0aa2cb0c987e1fa68ab9162afd4
StPalatinus <PalaCisco@gmail.com>
2021-02-08 07:56:52 +0300

    make changes to task add, make component statefull

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,6 +5,47 @@ export default class Header extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            newTaskValue: "",
+        }
+        
+        this.onButtonEnter = (e) => {
+
+            if (e.charCode === 13 &&   e.target.value === "") {
+                return;
+            }
+            if (e.charCode === 13){
+                this.props.onTaskAdd(e.target.value);
+                e.target.value = "";
+            } else {
+                this.setState = (e) => {
+                    this.props.onTaskAdd(e.target.value);
+                }
+            }
+        }
+      }
+
+      render() {
+        return (
+            <header className="header">
+                <h1>todos</h1>
+                <input className="new-todo" 
+                        placeholder = "What needs to be done?" 
+                        onKeyPress = {this.onButtonEnter}
+                        autoFocus 
+                        // value = {this.state.newTaskValue} 
+                        />
+            </header>
+        );
+    }
+}
+
+
+/*
+    export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
         let text = "";
         
         this.onButtonEnter = (e) => {
@@ -30,8 +71,10 @@ export default class Header extends React.Component {
                 <input className="new-todo" 
                         placeholder = "What needs to be done?" 
                         onKeyPress = {this.onButtonEnter}
-                        autoFocus/>
+                        autoFocus 
+                        value = {this.state.newTasValue} />
             </header>
         );
     }
 }
+*/
