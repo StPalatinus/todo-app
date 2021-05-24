84290682a2e2712467731c4ed452905e18bc995f
StPalatinus <PalaCisco@gmail.com>
2021-05-24 22:06:32 +0300

    reworked task-field.js, error in console

--- src/components/task-list/task/new-task-form/new-task-form.js
+++ src/components/task-list/task/new-task-form/new-task-form.js
@@ -1,4 +1,53 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import './new-task-form.css';
+
+const NewTaskForm = (props) => {
+  const taskInput = useRef(null);
+
+  const { description, onTaskChange, id, deleteEmptyTask } = props;
+
+  NewTaskForm.defaultProps = {
+    deleteEmptyTask: () => {},
+  };
+
+  const [newTaskValue, setNewTaskValue] = useState(description);
+
+  const onButtonEnter = (evt) => {
+    setNewTaskValue(evt.target.value);
+  };
+
+  const changeTask = (evt) => {
+    evt.preventDefault();
+    onTaskChange(newTaskValue, id);
+
+    if (newTaskValue === '') {
+      deleteEmptyTask(id);
+    }
+  };
+
+  useEffect(() => {
+    taskInput.current.focus();
+  }, []);
+
+  return (
+    <form onSubmit={changeTask}>
+      <input type="text" className="edit" ref={taskInput} value={newTaskValue} onChange={onButtonEnter} />
+    </form>
+  );
+};
+
+NewTaskForm.propTypes = {
+  description: PropTypes.string.isRequired,
+  onTaskChange: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
+  deleteEmptyTask: PropTypes.func,
+};
+
+export default NewTaskForm;
+
+/*
+  import React from 'react';
 import PropTypes from 'prop-types';
 import './new-task-form.css';
 
@@ -60,3 +109,5 @@ NewTaskForm.propTypes = {
 };
 
 export default NewTaskForm;
+
+*/
--- src/components/task-list/task/task-field/task-field.js
+++ src/components/task-list/task/task-field/task-field.js
@@ -16,29 +16,19 @@ const TaskField = (props) => {
   };
 
   const [createTime] = useState(created);
-  // const [tickCount, setTickCount] = useState(0);
   const [workTimerID, setworkTimerID] = useState(null);
   const [timerPlay, settimerPlay] = useState(false);
-  // const [workTime, setWorkTime] = useState(propsWorkTime);
   const [workTime, setWorkTime] = useReducer((state, isTimerPlay) => {
-    console.log(state);
     if (isTimerPlay) {
       onTimerTick(state + 1);
       return state + 1;
     }
-    return null;
+    return state;
   }, propsWorkTime);
 
   const timerStart = () => {
     clearInterval(workTimerID);
     settimerPlay(true);
-    // const newWorkTimerID = setInterval(() => {
-    //   onTimerTick(workTime);
-    //   setWorkTime((timerTime) => timerTime + 1);
-    //   console.log(workTime);
-    // }, 1000);
-    // settimerPlay(true);
-    // setworkTimerID(workTimerID);
   };
 
   const timerStop = () => {
@@ -48,12 +38,12 @@ const TaskField = (props) => {
 
   const onCheck = () => {
     props.ontaskCompleteStateToggle();
-    timerStop();
+    // timerStop();
   };
 
   const onTaskFieldFocus = () => {
     props.onEdit();
-    timerStop();
+    // timerStop();
   };
 
   const deleteTask = () => {
@@ -68,16 +58,13 @@ const TaskField = (props) => {
     return `${hours}:${minutes}:${seconds}`;
   };
 
-  const tick = () => {
-    // const newTick = tickCount + 1;
-    // setTickCount(newTick + 1);
-  };
+  const tick = () => {};
 
   useEffect(() => {
     const taskStartTimerID = setInterval(() => tick(), 30000);
     return () => {
       clearInterval(taskStartTimerID);
-      timerStop();
+      // timerStop();
     };
   }, []);
 
@@ -85,7 +72,6 @@ const TaskField = (props) => {
     if (timerPlay) {
       const newWorkTimerID = setInterval(() => {
         setWorkTime(workTime + 1);
-        console.log(workTime);
       }, 1000);
       setworkTimerID(newWorkTimerID);
     }
@@ -94,8 +80,7 @@ const TaskField = (props) => {
       clearInterval(workTimerID);
       settimerPlay(false);
     };
-  }, [timerPlay]);
-  // }, [ timerPlay, workTime ]);
+  }, [timerPlay, settimerPlay]);
 
   let timerComponent;
 
@@ -151,147 +136,3 @@ TaskField.propTypes = {
 };
 
 export default TaskField;
-
-/*
-import React from 'react';
-import PropTypes from 'prop-types';
-import formatDistanceToNow from 'date-fns/formatDistanceToNow';
-// import toDate from 'date-fns/toDate'
-import './task-field.css';
-
-class TaskField extends React.Component {
-  constructor(props) {
-    super(props);
-
-    const { created, onTimerTick, workTime } = this.props;
-
-    TaskField.defaultProps = {
-      description: 'Default task',
-      completed: false,
-      onDelete: () => {},
-      onEdit: () => {},
-      ontaskCompleteStateToggle: () => {},
-      workTime: 0,
-    };
-
-    this.state = {
-      createTime: created,
-      workTime,
-      tickCount: 0,
-    };
-
-    this.onCheck = () => {
-      this.props.ontaskCompleteStateToggle();
-      this.timer.stop();
-    };
-
-    this.onTaskFieldFocus = () => {
-      this.props.onEdit();
-    };
-
-    this.timer = {
-      workTimerID: null,
-      start: () => {
-       clearInterval(this.workTimerID);
-        this.workTimerID = setInterval(() => {
-          onTimerTick(this.state.workTime);
-          this.setState((state) => {
-            const newTime = state.workTime + 1;
-
-            return {
-              workTime: newTime,
-            };
-          });
-        }, 1000);
-      },
-      stop: () => {
-        clearInterval(this.workTimerID);
-      },
-    };
-
-    this.formatTime = (timeInSeconds) => {
-      const hours = Math.floor(timeInSeconds / 60 / 60);
-      const minutes = Math.floor(timeInSeconds / 60);
-      const seconds = timeInSeconds - minutes * 60 - hours * 60 * 60;
-      return `${hours}:${minutes}:${seconds}`;
-    };
-  }
-
-  componentDidMount() {
-    this.taskStartTimerID = setInterval(() => this.tick(), 30000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.taskStartTimerID);
-    this.timer.stop();
-  }
-
-  tick() {
-    const newTick = this.state.tickCount + 1;
-    this.setState(() => ({
-      tickCount: newTick,
-    }));
-  }
-
-  render() {
-    const { description, completed } = this.props;
-    const { workTime } = this.state;
-    let timer;
-
-    if (!completed) {
-      timer = (
-        <span className="timer">
-          <button className="icon icon-play" type="button" aria-label="Play" onClick={this.timer.start}>
-            ▶
-          </button>
-          <button className="icon icon-pause" type="button" aria-label="Pause" onClick={this.timer.stop}>
-            ⏸
-          </button>
-          <span className="timer--elapsed-time">{this.formatTime(workTime)}</span>
-        </span>
-      );
-    } else {
-      timer = (
-        <span className="timer">
-          <button className="icon icon-play" type="button" aria-label="Play" onClick={() => {}}>
-            ▶
-          </button>
-          <button className="icon icon-pause" type="button" aria-label="Pause" onClick={() => {}}>
-            ⏸
-          </button>
-          <span className="timer--elapsed-time">{this.formatTime(workTime)}</span>
-        </span>
-      );
-    }
-
-    return (
-      <div className="view">
-        <input className="toggle" type="checkbox" checked={completed} onChange={this.onCheck} />
-        <label>
-          <span className="description">{description}</span>
-          {timer}
-          <span className="created">{`${formatDistanceToNow(
-            new Date(this.state.createTime),
-            'MM/dd/yyyy'
-          )}  ago`}</span>
-        </label>
-        <button className="icon icon-edit" onClick={this.props.onEdit} type="button" aria-label="Edit" />
-        <button className="icon icon-destroy" onClick={this.props.onDelete} type="button" aria-label="Remove" />
-      </div>
-    );
-  }
-}
-
-TaskField.propTypes = {
-  completed: PropTypes.bool,
-  created: PropTypes.instanceOf(Date).isRequired,
-  description: PropTypes.string,
-  onDelete: PropTypes.func,
-  onEdit: PropTypes.func,
-  ontaskCompleteStateToggle: PropTypes.func,
-  onTimerTick: PropTypes.func.isRequired,
-  workTime: PropTypes.number,
-};
-
-export default TaskField;
-*/
